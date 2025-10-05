97a52a3611a8a501356ee76c6dd8ebdbd11d0542
solorpla <putrisolo517@gmail.com>
2025-10-05 21:57:09 +0700

    ok

--- app/Http/Controllers/kepalaSekolah/kepalaSekolahDashboardController.php
+++ app/Http/Controllers/kepalaSekolah/kepalaSekolahDashboardController.php
@@ -10,7 +10,7 @@ class kepalaSekolahDashboardController extends Controller
     public function index()
     {
         $data = [
-           'content' => 'layouts.guru.content',
+           'content' => 'kepalaSekolah.dashboard.index',
             'title' => 'Dashboard'
         ];
 
